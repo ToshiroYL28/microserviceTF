56990a80511247016c1e53969346db12f5abd109
David <pablodavidhuamani@gmail.com>
2022-06-21 23:13:54 -0500

    update little

--- appbient-microservice-usuarios/src/main/java/com/example/appbient_microservice_usuarios/AppbientMicroserviceUsuariosApplication.java
+++ appbient-microservice-usuarios/src/main/java/com/example/appbient_microservice_usuarios/AppbientMicroserviceUsuariosApplication.java
@@ -1,10 +1,16 @@
 package com.example.appbient_microservice_usuarios;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@OpenAPIDefinition
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableEurekaClient
 public class AppbientMicroserviceUsuariosApplication {
 
     public static void main(String[] args) {
