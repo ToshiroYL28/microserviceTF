a7503e2f8490e8a612ac85dfcdcb80c8da67994d
ToshiroYL28 <u201514153@upc.edu.pe>
2022-07-04 17:33:10 -0500

    first commit

--- microservice/src/main/java/com/example/microservice/MicroserviceApplication.java
+++ microservice/src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.appbient_microservice_foro;
+package com.example.microservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
